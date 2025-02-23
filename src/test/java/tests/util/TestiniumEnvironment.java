35c492850b0da10e78d6dcc2f8c30a0f558853a3
Furkan Kartal <Furkan.kartal@testinium.com>
2025-02-23 20:01:18 +0300

    Change env to property

--- src/test/java/tests/util/TestiniumEnvironment.java
+++ src/test/java/tests/util/TestiniumEnvironment.java
@@ -23,7 +23,7 @@ public class TestiniumEnvironment {
         System.out.println("sessionId:"+ sessionId);
 
         if (envProfile.equals("testinium")) {
-            sessionId = System.getenv("deviceParkSessionId") != null ? System.getenv("sessionId") : "ad70ba7e-1b42-470f-9393-035cdd7569e6";
+            sessionId = System.getProperty("deviceParkSessionId") != null ? System.getProperty("sessionId") : "ad70ba7e-1b42-470f-9393-035cdd7569e6";
             appiumVersion = System.getenv("appiumVersion") != null ? System.getenv("appiumVersion") : "2.5.4";
             takeScreenRecording = System.getenv("takeScreenRecording") != null ? System.getenv("takeScreenRecording") : "true";
             app = System.getenv("app") != null ? System.getenv("app") : "null";
