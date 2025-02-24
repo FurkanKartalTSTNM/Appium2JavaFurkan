a3f16b2c323e6641e47011f3de15119ea7d0fd51
Furkan Kartal <Furkan.kartal@testinium.com>
2025-02-24 19:26:18 +0300

    Change property file

--- src/test/java/tests/ConfigReader.java
+++ src/test/java/tests/ConfigReader.java
@@ -8,7 +8,7 @@ public class ConfigReader {
 
     public String getPropertyValue(String key) {
         Properties properties = new Properties();
-        try (FileInputStream input = new FileInputStream("src/test/resources/application.properties")) {
+        try (FileInputStream input = new FileInputStream("env/default/default.properties")) {
             properties.load(input);
             return properties.getProperty(key);
         } catch (IOException e) {
